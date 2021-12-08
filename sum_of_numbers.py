59ee13920da3f661fd3e9c30501dfd7475a765ce
stephen-kovolski <s.kovolski@gmail.com>
2021-12-08 17:35:44 -0500

    completed using a fuction w/ a cli

--- sum_of_numbers.py
+++ sum_of_numbers.py
@@ -4,6 +4,13 @@
 arg1 = input("what number would you like to start with?\n")
 arg2 = input("what is the second number you would like?\n")
 
-print("The sum of the two numbers you requested is", int(arg1) + int(arg2))
+#print("The sum of the two numbers you requested is", int(arg1) + int(arg2))
+
+def add_number(arg1, arg2):
+    sum = int(arg1) + int(arg2)
+
+    print("The sum of the two numbers you requested is", sum)
+
+add_number(arg1, arg2)
 
 
