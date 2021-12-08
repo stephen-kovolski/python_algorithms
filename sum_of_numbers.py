d107a8d37a2124e1f1d28b3b7fc9f2b7fb361039
stephen-kovolski <s.kovolski@gmail.com>
2021-12-08 17:34:28 -0500

    completed w/o a function

--- sum_of_numbers.py
+++ sum_of_numbers.py
@@ -1,18 +1,9 @@
-#Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
-#Note: a and b are not ordered!
-
-#examples
-#(1, 0) --> 1 (1 + 0 = 1)
-#(1, 2) --> 3 (1 + 2 = 3)
-#(0, 1) --> 1 (0 + 1 = 1)
-#(1, 1) --> 1 (1 since both are same)
-#(-1, 0) --> -1 (-1 + 0 = -1)
-#(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+#Given two integers a and b, which can be positive or negative, find the sum of all the integers 
 
 
-def add_number(arg1, arg2):
-    sum = arg1 + arg2
+arg1 = input("what number would you like to start with?\n")
+arg2 = input("what is the second number you would like?\n")
 
-    print(sum)
+print("The sum of the two numbers you requested is", int(arg1) + int(arg2))
 
-add_number(-1, 2)
+
