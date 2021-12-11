34b8005b7660dbe7f935b3eac72073b8c1cecc2c
stephen-kovolski <s.kovolski@gmail.com>
2021-12-10 20:05:47 -0500

    verifying int's

--- atm_pin.py
+++ atm_pin.py
@@ -2,10 +2,15 @@
 
 #If the function is passed a valid PIN string, return true, else return false.
 
+#Need to verify that there are only int's in the pin and reject it if there is anything else entered.
+
+
 
 pin = input("Please enter your pin to continue\n")
 
 if len(pin) == 4:
+    converted_pin = int(pin)
+    print(type(pin))
     print("You've entered a four digit pin")
 elif len(pin) == 6:
     print("You've entered a 6 digit pin")
