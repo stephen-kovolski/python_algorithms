d114ceea05dcc95834ed77e93359a5761003dd06
stephen-kovolski <s.kovolski@gmail.com>
2022-02-03 22:45:02 -0500

    reverse str

---
+++ reverse_a_string.py
@@ -0,0 +1,14 @@
+#Reverse the provided string.
+#You may need to turn the string into an array before you can reverse it.
+
+#"hello"
+#"howdy"
+#"greetings from earth"
+
+from audioop import reverse
+
+
+list = 'howdy'
+print(list[::-1])
+
+    
