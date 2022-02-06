e9471ed2427a2a3c3b3ce26ec07ba45bc7d8e16e
stephen-kovolski <s.kovolski@gmail.com>
2022-02-05 23:41:53 -0500

    funcs

--- reverse_a_string.py
+++ reverse_a_string.py
@@ -3,15 +3,23 @@ greeting = input('Hello!  What would you like reveresed?\n')
 list = greeting[::-1]
 print(list)
 
-closing = input("would you like to try again?\n")
+def closing():
+    closing_stmt = input("would you like to try again?\n")
 
-if closing == 'yes':
+
+def play():
     again = input("Great! What would you like reversed?\n")
     list_2 = again[::-1]
     print(list_2)
-    closing
-    
+    closing()
+
+
+
+if closing == 'yes':
+    play()    
 else:
     print("Thanks for playing!")
 
     
+
+
