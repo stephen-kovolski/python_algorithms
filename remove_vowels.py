68ab3693412f791d26fd6a51d9f9bedbbcd79337
stephen-kovolski <s.kovolski@gmail.com>
2022-02-07 23:13:58 -0500

    loop

--- remove_vowels.py
+++ remove_vowels.py
@@ -1,10 +1,10 @@
-"""
-Trolls are attacking your comment section!
-A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
-Your task is to write a function that takes a string and return a new string with all vowels removed.
-For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
-"""
 
 
+
+comment_one = ["this is the comment"]
+
+for letters in comment_one:
+    new_string = [letters]
+    print(new_string)
 
 
