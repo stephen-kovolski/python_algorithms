f01757f2605a0f859334433934e32ebcd481d084
stephen-kovolski <s.kovolski@gmail.com>
2022-02-08 23:42:06 -0500

    dictionary

--- remove_vowels.py
+++ remove_vowels.py
@@ -1,12 +1,19 @@
 
-
+import re
+from tkinter import E
 
 comment_one = ["this is the comment"]
-vowels = ['a', 'e', 'i', 'o', 'u']
+vowels = {
+    'A': 'a',
+    'E': 'e',
+    'I': 'i',
+    'O': 'o',
+    'U': 'u'
+}
 new_comment = ""
 
-for letters in comment_one:
-    comment_one.remove(aeiou)
+for vowels in comment_one:
+    comment_one.remove(vowels)
     print(comment_one)
-
+    print(vowels)
 
