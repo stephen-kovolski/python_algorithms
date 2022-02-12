83b95dcae8ede9aab204dfbebdc8d8b7b58436a3
stephen-kovolski <s.kovolski@gmail.com>
2022-02-11 20:38:30 -0500

    ...

--- slice_and_splice.py
+++ slice_and_splice.py
@@ -4,9 +4,7 @@
 #Return the resulting array. The input arrays should remain the same after the function runs.
 
 def frankenSplice(arr1, arr2, n):
-
-
     
-  return arr2
+    return arr2
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1)
