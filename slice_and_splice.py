a39b128f549164d3a1f739271b706f0ac2781759
stephen-kovolski <s.kovolski@gmail.com>
2022-02-21 20:10:10 -0500

    slice

--- slice_and_splice.py
+++ slice_and_splice.py
@@ -4,9 +4,16 @@
 #Return the resulting array. The input arrays should remain the same after the function runs.
 
 def frankenSplice(arr1, arr2, n):
-    for i in arr1:
-        print(i)
-    for j in arr2:
-        print(j)
-    print(arr2.splice(arr1[n:]))
+    arr1[:n] = arr2
+    print(arr1)
+    print(arr2)
+
+
+
+    #for i in arr1:
+        #print(i)
+    #for j in arr2:
+     #   new_list = j
+      #  print(new_list)
+       # #print(arr2.insert(n, arr1))
 frankenSplice([1, 2, 3], [4, 5, 6], 1)
