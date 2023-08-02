b179281d42a11e5a262248502c8ba80555e000dd
stephen-kovolski <s.kovolski@gmail.com>
2023-08-01 21:58:30 -0400

    solved

---
+++ your_life_in_weeks.py
@@ -0,0 +1,11 @@
+
+
+age = input('What is your current age?')
+
+years_left = 90 - float(age)
+months_left = 1080 - (float(age) * 12)
+days_left = 32850 - (float(age) * 365)
+
+print(years_left) 
+print(months_left) 
+print(days_left) 
