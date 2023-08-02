6cd0f8080ea06c4e9bf69ad0b533722c1d235952
stephen-kovolski <s.kovolski@gmail.com>
2023-08-02 01:20:51 -0400

    felicias fruits

---
+++ py_basics.py
@@ -0,0 +1,22 @@
+banana = .5
+orange = 1
+blueberries = 2
+strawberries = 2
+peaches = 1
+
+carrot = 1
+garlic = .5
+onion = 1
+potato = 1.5
+pepper = 1
+
+
+def my_function(num1, num2):
+  input("welcome to Felicia's Fruits! Here is what we have per pound. What would you like?\n banana .5 lb,\n orange 1 lb,\n blueberries 2 lb,\n strawberries 2 lb,\n peaches 1 lb,\n carrot 1 lb,\n garlic .5 lb,\n onion 1 lb,\n potato 1.5 lb,\n pepper 1 lb")
+
+my_function(5, 10)
+
+
+
+
+
