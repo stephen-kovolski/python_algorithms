0325ec6bda65fab483b39b9153afa6a73fad4cbb
stephen-kovolski <s.kovolski@gmail.com>
2023-08-03 22:26:06 -0400

    solved

---
+++ roller_coaster_tickets.py
@@ -0,0 +1,6 @@
+height = float(input('Welcome to the roller coaster!\nHow tall are you in cm?\n'))
+
+if height >= 120:
+    print('You can ride the roller coaster')
+else:
+    print('sorry you need to grow taller to enjoy these rides. ')
