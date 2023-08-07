e6711016f466c6669fa670123a45c592ba1adaa5
stephen-kovolski <s.kovolski@gmail.com>
2023-08-06 23:24:17 -0400

    band name generator

---
+++ band_name_generator.py
@@ -0,0 +1,10 @@
+# asks the name of city you grew up in
+# then asks the name of your pet
+# combines them and gives you a new name suggestion
+
+
+
+city = input('Welcome to the band name genrator!\nWhat city or town did you grow up in?\n')
+pet = input("what was your childhood pets name?\n")
+
+print(f"a great name you could call your band is {city} {pet}")
