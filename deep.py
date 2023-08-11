19f92e4be4f29e3e26381bb780e7b4590ec8af20
stephen-kovolski <s.kovolski@gmail.com>
2023-08-11 17:25:21 -0400

    project 1

---
+++ deep.py
@@ -0,0 +1,8 @@
+def main():
+    while True:
+        a = input('What is the answer to the Great Question of Life, the Universe and Everything?\n')
+        int(a)
+        print(a)
+        break
+
+main()
