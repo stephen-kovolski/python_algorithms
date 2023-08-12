86f7682ddbf85cf16d5631c01d1ed73fe9964281
stephen-kovolski <s.kovolski@gmail.com>
2023-08-12 17:26:02 -0400

    practice

--- py_basics.py
+++ py_basics.py
@@ -1,19 +1,23 @@
-banana = .5
-orange = 1
-blueberries = 2
-strawberries = 2
-peaches = 1
+inventory= {
 
-carrot = 1
-garlic = .5
-onion = 1
-potato = 1.5
-pepper = 1
+'banana': .5,
+'orange': 1,
+'blueberries': 2,
+'strawberries': 2,
+'peaches': 1,
 
+'carrot': 1,
+'garlic': .5,
+'onion': 1,
+'potato': 1.5,
+'pepper': 1
 
+}
 
-input("welcome to Felicia's Fruits! Here is what we have and the price per pound. What would you like?\n banana .5 lb,\n orange 1 lb,\n blueberries 2 lb,\n strawberries 2 lb,\n peaches 1 lb,\n carrot 1 lb,\n garlic .5 lb,\n onion 1 lb,\n potato 1.5 lb,\n pepper 1 lb\n")
+orange = inventory.orange 
 
+#input("welcome to Felicia's Fruits! Here is what we have and the price per pound. What would you like?\n banana .5 lb,\n orange 1 lb,\n blueberries 2 lb,\n strawberries 2 lb,\n peaches 1 lb,\n carrot 1 lb,\n garlic .5 lb,\n onion 1 lb,\n potato 1.5 lb,\n pepper 1 lb\n")
+print(inventory.orange)
 
 
 
