efac88fd2034707b85658f26f2754a4a95ea3baa
Stephen Kovolski <s.kovolski@gmail.com>
2023-11-15 18:35:11 -0500

    working on v2

--- bmi_calc_2.py
+++ bmi_calc_2.py
@@ -1,3 +1,6 @@
+# have to add the levels of bmi
+
+
 height = input("what is your height in meters?\n")
 weight = input("what is your weight in kg?\n")
 print("Your BMI is ", (float(weight) // float(height) ** 2))
