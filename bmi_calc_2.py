1c970cd01141d204ba960ef0c0e0914b12087df2
Stephen Kovolski <s.kovolski@gmail.com>
2023-11-17 20:55:34 -0500

    solved

--- bmi_calc_2.py
+++ bmi_calc_2.py
@@ -3,4 +3,4 @@
 
 height = input("what is your height in meters?\n")
 weight = input("what is your weight in kg?\n")
-print("Your BMI is ", (float(weight) // float(height) ** 2))
+print("Your BMI is ", (float(weight) / float(height) * 2))
